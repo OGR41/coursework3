5d1b2675cad377679b04eb6b94b1c377eb4b4a4a
OSR41 <cheplygin88@gmail.com>
2023-02-13 18:08:58 +1200

    .

--- code.py
+++ code.py
@@ -1,32 +1,32 @@
-import requests
-from pprint import pprint
 from datetime import datetime
+import requests
+
+# загружаем и сохраняем в переменную файл json
+get_json = requests.get('https://www.jsonkeeper.com/b/1IUI').json()
 
 
-operations_load = requests.get('https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d22c7143-d55e-4f1d-aa98'
-                           '-e9b15e5e5efc/operations.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256'
-                           '=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230208%2Fus-west'
-                           '-2%2Fs3%2Faws4_request&X-Amz-Date=20230208T071504Z&X-Amz-Expires=86400&X-Amz-Signature'
-                           '=f2483ac8aa953b57e70ee8d0eb97ccd40598723d6153ea371feae73945be682d&X-Amz-SignedHeaders'
-                           '=host&response-content-disposition=filename%3D%22operations.json%22&x-id=GetObject')
-operations_json = operations_load.json()
+def last_operations(json_):
+    '''
+    :param json_: получает данные из json-файла
+    :return: список с отформатированной информацией о последних 5-ти операциях
+    '''
+    executed_list = []      # список с операциями по фильтрам 'state'
 
-
-
-def last_operations():
-    executed_list = []
-    for i in operations_json:
-        operations_index = operations_json.index(i)
+    for i in get_json:      # цикл для фильтрации списка по 'state'
+        operations_index = get_json.index(i)
         if "state" not in i:
             continue
-        elif operations_json[operations_index]['state'] == 'EXECUTED':
+        elif get_json[operations_index]['state'] == 'EXECUTED':
             executed_list.append(i)
             if "from" not in i:
-                operations_json[operations_index]['from'] = "Данные об отравителе отсутствуют"
+                get_json[operations_index]['from'] = "Данные об отравителе отсутствуют"
 
-    executed_list.sort(key=lambda x: x['date'])
-    last_five_operations = []
-    for i in list(reversed(executed_list)):
+    executed_list.sort(key=lambda x: x['date'])     # сортируем список по дате
+    last_five_operations = []       # список с последними 5-ю операциями
+    a = []      # список с отформатированными операциями
+
+    for i in list(reversed(executed_list)):     # цикл для добавления в список определённых параметров(дата, ->
+                                                # -> название операции, отправитель, получатель, сумма, валюта)
         if len(last_five_operations) < 5:
             last_five_operations.append(i)
             last_five_operations_index = last_five_operations.index(i)
@@ -37,17 +37,15 @@ def last_operations():
             amount = last_five_operations[last_five_operations_index]['operationAmount']['amount']
             currency = last_five_operations[last_five_operations_index]['operationAmount']['currency']['name']
             if 'Счет' in from_:
-                print(f'{thedate.strftime("%d.%m.%Y")} {description}\n'
-                      f'{from_[0:-20] + "*" * len(from_[-6:-4]) + from_[-4:]} -> '
-                      f'{to[0:-20] + "*" * len(to[-6:-4]) + to[-4:]}\n{amount} {currency}\n')
+                a.append(f'{thedate.strftime("%d.%m.%Y")} {description}\n{from_[0:-20] + "*" * len(from_[-6:-4]) + from_[-4:]} -> {to[0:-20] + "*" * len(to[-6:-4]) + to[-4:]}\n{amount} {currency}\n')
+
             elif 'Данные об отравителе отсутствуют' in from_:
-                print(f'{thedate.strftime("%d.%m.%Y")} {description}\n'
-                      f'{from_} -> {to[0:-20] + "*" * len(to[-6:-4]) + to[-4:]}\n{amount} {currency}\n')
+                a.append(f'{thedate.strftime("%d.%m.%Y")} {description}\n{from_} -> {to[0:-20] + "*" * len(to[-6:-4]) + to[-4:]}\n{amount} {currency}\n')
+
             else:
-                print(f'{thedate.strftime("%d.%m.%Y")} {description}\n'
-                      f'{from_[0:-16] + from_[-16:-12] + " " + from_[-12:-10] + "*" * len(from_[-10:-8]) + " " + "*" * len(from_[-8:-4]) + " " + from_[-4:]} ->'
-                      f'{to[0:-20] + "*" * len(to[-6:-4]) + to[-4:]}\n{amount} {currency}\n')
+                a.append(f'{thedate.strftime("%d.%m.%Y")} {description}\n{from_[0:-16] + from_[-16:-12] + " " + from_[-12:-10] + "*" * len(from_[-10:-8]) + " " + "*" * len(from_[-8:-4]) + " " + from_[-4:]} -> {to[0:-20] + "*" * len(to[-6:-4]) + to[-4:]}\n{amount} {currency}\n')
+    return '\n'.join(a)
 
 
-last_operations()
+print(last_operations(get_json))
 
--- utils.py
+++ utils.py
@@ -1,14 +1,22 @@
 import requests
+from pprint import pprint
 
 
 def get_data(url):
     try:
-        response = requests.get(url)
+        response = requests.request("GET", url)
         if response.status_code == 200:
             return response.json(), "INFO: Данные успешно получены"
         return None, f"ERROR: status_code: {response.status_code}"
+
     except requests.exceptions.ConnectionError:
         return None, "ERROR: requests.exceptions.ConnectionError"
+
     except requests.exceptions.JSONDecodeError:
-        print(response.url)
         return None, "ERROR: requests.exceptions.JSONDecodeError"
+
+
+def get_filter_data(data):
+    data = [x for x in data if 'state' in x and x['state'] == 'EXECUTED']
+    return data
+
